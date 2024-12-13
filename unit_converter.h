516f7926f40ec0c4dc8abdf147d9a045e6f13abc
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2024-12-13 20:59:00 -0300

    Conversor de comprimento

---
+++ unit_converter.h
@@ -0,0 +1,30 @@
+void comprimento(){
+
+    int tipo;
+
+        printf("Qual unidade você quer converter? ");
+        printf("1 - metro");
+        printf("2 - centímetro");
+        printf("3 - milímetro");
+        scanf("%d", &tipo);//Usuário digita itemEscolhido
+        fflush(stdin);//Limpa o buffer
+
+        switch(tipo){
+            case 1:{
+                //converter metros para cm e mm
+                print("Valor em cm: %f", );
+                break;
+            }
+            case 2:{
+                //converter centimetro para m e mm
+                print("Valor em cm: %f", );
+                break;
+            }
+            case 1:{
+                //converter milimetros para m e cm
+                print("Valor em cm: %f", );
+                break;
+            }
+    }
+
+}
